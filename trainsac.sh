9e76cb70c343c98b36a69213348ce535e34b16df
Igreene5290 <113941127+Igreene5290@users.noreply.github.com>
2025-04-14 14:17:45 -0400

    Add files via upload

---
+++ trainsac.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH --gpus-per-node=1
+#SBATCH -e slurm-%j.err
+#SBATCH -o slurm-%j.out
+
+cd ~/MicroRTS-Py
+
+source .venv/bin/activate
+export JAVA_HOME=/usr/lib/jvm/jre/
+
+uv pip install -r requirements.txt
+pwd > .venv/lib/python3.9/site-packages/gym_microrts.pth
+python hello_world.py
+
+cd experiments
+python sac3.py
